61518d6a5569c9bac9974ed57c8dadc3ae26363d
Agung Paul <33806153+AgungPaul9@users.noreply.github.com>
2018-09-25 16:53:57 +0700

    Update formawal.php

--- formawal.php
+++ formawal.php
@@ -1,5 +1,27 @@
-<?php
-
-
-
-?>
+<!DOCTYPE html>
+<html>
+<head>
+	<title>TUGAS</title>
+</head>
+<body>
+ <form action="proses.php" method="POST">
+ 	<table>
+ 		<tr>
+ 			<td>USERNAME</td>
+ 			<td>:</td>
+ 			<td><input type="text" name="username"></td>
+ 		</tr>
+ 		<tr>
+ 			<td>PASSWORD</td>
+ 			<td>:</td>
+ 			<td><input type="password" name="password"></td>
+ 		</tr>
+ 		<tr>
+ 			<td></td>
+ 			<td></td>
+ 			<td><input type="submit" name="submit"></td>
+ 		</tr>
+ 	</table>
+ </form>
+</body>
+</html>
