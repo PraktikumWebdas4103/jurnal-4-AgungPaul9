eada87f100eb57f7e5836531d4fc391d551dc1a8
Agung Paul <33806153+AgungPaul9@users.noreply.github.com>
2018-09-25 16:54:36 +0700

    Update proses.php

--- proses.php
+++ proses.php
@@ -1,4 +1,45 @@
+<?php 
+	if (isset($_POST['submit'])) {
+		$username = $_POST['username'];
+		$password = $_POST['password'];
+		# code...
+	}
+?>
+<!DOCTYPE html>
+<html>
+<head>
+	<title>TUGAS</title>
+</head>
+<body>
+ <form>
+ 	<table>
+ 		<tr>
+ 			<td>Nama</td>
+ 			<td>:</td>
+ 			<td><?php echo $username; ?></td>
+ 		</tr>
+ 		<tr>
+ 			<td>Hobi</td>
+ 			<td>:</td>
+ 			<td>
+ 				<input type="checkbox" name="vehicle1" value="Bike">bike<br>
+				<input type="checkbox" name="vehicle2" value="Car">car<br>
+				<input type="checkbox" name="vehicle3" value="Boat">boat<br>
+			</td>
+ 		</tr>
+ 		<tr>
+ 			<td>Gambar</td>
+ 			<td>:</td>
+ 			<td><input type="file" name="pictures"></td>
+ 		</tr>
+ 		<tr>
+ 			<td></td>
+ 			<td></td>
+ 			<td><input type="submit" name="submit_"></td>
+ 		</tr>
+ 	</table>
+ </form>
+</body>
+</html>
 <?php
-
-
 ?>
